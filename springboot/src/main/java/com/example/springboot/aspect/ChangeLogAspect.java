3ec15f57ae786ee2804a7e2abe6aa27bf4905da7
tanchusheng <tanchusheng@zto56.com>
2022-09-26 22:39:02 +0800

    变更日志

--- springboot/src/main/java/com/example/springboot/aspect/ChangeLogAspect.java
+++ springboot/src/main/java/com/example/springboot/aspect/ChangeLogAspect.java
@@ -1,16 +1,13 @@
 package com.example.springboot.aspect;
 
-import com.example.springboot.mapper.SysUserMapper;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.binding.MapperProxyFactory;
+import org.apache.ibatis.binding.MapperMethod;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -21,12 +18,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * @author tanchusheng
  * @version 1.0
- * @description: 变更日志切面
+ * @description: 前置通知 变更日志切面
  * @date: 2022/9/26 13:14
  */
 
@@ -47,11 +43,25 @@ public class ChangeLogAspect {
     public void pointCut() {
     }
 
+    /**
+     * 前置通知：目标方法执行之前执行以下方法体的内容。
+     * value：绑定通知的切入点表达式。可以关联切入点声明，也可以直接设置切入点表达式
+     * <br/>
+     * * @param joinPoint：提供对连接点处可用状态和有关它的静态信息的反射访问<br/> <p>
+     * * * Object[] getArgs()：返回此连接点处（目标方法）的参数，目标方法无参数时，返回空数组
+     * * * Signature getSignature()：返回连接点处的签名。
+     * * * Object getTarget()：返回目标对象
+     * * * Object getThis()：返回当前正在执行的对象
+     * * * StaticPart getStaticPart()：返回一个封装此连接点的静态部分的对象。
+     * * * SourceLocation getSourceLocation()：返回与连接点对应的源位置
+     * * * String toLongString()：返回连接点的扩展字符串表示形式。
+     * * * String toShortString()：返回连接点的缩写字符串表示形式。
+     * * * String getKind()：返回表示连接点类型的字符串
+     * * * </p>
+     */
+    @Before("pointCut()")
+    public Object around(JoinPoint point) throws Throwable {
 
-    @Around("pointCut()")
-    public Object around(ProceedingJoinPoint point) throws Throwable {
-
-        Object proceed = point.proceed();
         // 切入点所在目标对象
         Object target = point.getTarget();
         // 获取方法的参数
@@ -59,8 +69,6 @@ public class ChangeLogAspect {
 
         // 获取切入点方法的名字
         String methodName = point.getSignature().getName().toLowerCase();
-
-        Method[] declaredMethods = target.getClass().getDeclaredMethods();
 
         String objName = args[0].getClass().toString();
 
@@ -71,6 +79,8 @@ public class ChangeLogAspect {
         // 目标对象名
         String backupNameSpace = backupObjNameSections[backupObjNameSections.length - 1];
 
+        MethodSignature signature = (MethodSignature) point.getSignature();
+
         if (args[0] instanceof List) {
             // System.out.println("batch   operator  list");
             for (Object obj : (List) args[0]) {
@@ -79,84 +89,69 @@ public class ChangeLogAspect {
                 String[] tempBackupObjNameSections = tempBackupNameSpace.split("\\.");
                 backupNameSpace = tempBackupObjNameSections[tempBackupObjNameSections.length - 1];
 
-                backUpToLog(backupNameSpace, methodName, obj, point);
+                backUpToLog(backupNameSpace, methodName, obj, target, signature);
             }
         } else {
-            backUpToLog(backupNameSpace, methodName, args[0], point);
+            backUpToLog(backupNameSpace, methodName, args[0], target, signature);
         }
 
         return null;
     }
 
     /**
-     *
      * @param backupNameSpace 目标类名
-     * @param methodName 方法名
-     * @param args 参数
-     * @throws InstantiationException
+     * @param methodName      方法名
+     * @param args            参数
+     * @param target          目标对象
+     * @param signature       方法签名
      * @throws IllegalAccessException
      * @throws InvocationTargetException
      */
-    private void backUpToLog(String backupNameSpace, String methodName, Object args,ProceedingJoinPoint point) throws InstantiationException, IllegalAccessException, InvocationTargetException, ClassNotFoundException {
+    private void backUpToLog(String backupNameSpace, String methodName, Object args, Object target, MethodSignature signature) throws IllegalAccessException, InvocationTargetException {
         if (backUpList.contains(backupNameSpace)) {
-            if (methodName.indexOf("delete") != -1 || methodName.indexOf("update") != -1) {
+            if (methodName.contains("delete") || methodName.contains("update")) {
                 /**操作类型  1：修改  2 ：删除**/
                 Integer operatorType = 1;
-                if (methodName.indexOf("delete") != -1)
+                if (methodName.contains("delete"))
                     operatorType = 2;
-                Class DOClass = args.getClass();
-                Method[] methods = args.getClass().getDeclaredMethods();
 
-                Method getId = null;
-                Method setSiteId = null;
-                Method getSiteCode = null;
-                for (Method tmpMethod : methods) {
-                    if (tmpMethod.getName().indexOf("getId") != -1) {
-                        getId = tmpMethod;
-                        break;
-                    }
-                }
+                // 参数class
+                Class<?> argsClass = args.getClass();
+                Method[] argsMethods = argsClass.getDeclaredMethods();
 
-                String primaryKeyId = null;
-                Object paramObj = DOClass.newInstance();
+                // 目标class
+                Class<?> targetClass = target.getClass();
+                Method[] targetMethod = targetClass.getDeclaredMethods();
 
-
-                Method[] allMethods = DOClass.getDeclaredMethods();
-                String backupSelectId = backupNameSpace + ".selectByPrimaryKey";
-                SqlSession sqlSession = sqlSessionFactory.openSession();
-//                sqlSession.selectOne(paramObj.getId);
-                // 根据ID查询对象信息
-//
-                MethodSignature signature = (MethodSignature) point.getSignature();
                 Class<?> aClass = signature.getMethod().getDeclaringClass();
                 Method[] methods1 = aClass.getMethods();
-                List<Method> selectByPrimaryKey = Arrays.stream(methods1).filter(item -> item.getName().indexOf("selectByPrimaryKey") != -1).collect(Collectors.toList());
-                Method method = selectByPrimaryKey.get(0);
-                Object selectByPrimaryKey1 = method.invoke("selectByPrimaryKey", 1);
-                Mapper mapper = (Mapper) sqlSession.getMapper(aClass);
-//                MapperProxyFactory mapperProxyFactory = new MapperProxyFactory()
-                // public abstract int com.example.springboot.mapper.SysUserMapper.updateByPrimaryKeySelective(com.example.springboot.domain.SysUser)
-                Object backupObj = ((SysUserMapper)sqlSession.getMapper(aClass)).selectByPrimaryKey(1L);
-//                ((SysUserMapper)sqlSession.getMapper(((MethodSignature)point.getSignature()).getMethod().clazz)).selectByPrimaryKey(1L)
-//                Object backupObj = this.queryForObject(backupSelectId, paramObj);
-//                Object backupObj = new Object();
 
+                // 过滤出selectByPrimaryKey方法
+                List<Method> methodList = Arrays.stream(methods1).filter(item -> item.getName().contains("selectByPrimaryKey")).collect(Collectors.toList());
+                if (methodList.size() == 0) {
+                    return;
+                }
+                SqlSession sqlSession = sqlSessionFactory.openSession();
+                MapperMethod mapperMethod = new MapperMethod(target.getClass(), methodList.get(0), sqlSession.getConfiguration());
+                Object[] param = {args};
+                // 获取原来的对象
+                Object backupObj = mapperMethod.execute(sqlSession, param);
 
-                List<Method> originalMethod = filterField(allMethods);
-                List<Method> modifyMethod = filterField(args.getClass().getDeclaredMethods());
+                List<Method> originalMethod = filterField(argsMethods);
 
                 for (int i = 0; i < originalMethod.size(); i++) {
 
-                    Method original = originalMethod.get(i);
-                    Method modify = modifyMethod.get(i);
+                    Method method = originalMethod.get(i);
 
-                    String fieldName = original.getName().substring(3, original.getName().length()).toUpperCase();
+                    String fieldName = method.getName().substring(3).toUpperCase();
 
                     String originalValue = "";
                     String modifyValue = "";
 
-                    Object originalObj = original.invoke(backupObj);
-                    Object modifyObj = modify.invoke(args);
+                    Object originalObj = method.invoke(backupObj);
+                    Object modifyObj = method.invoke(args);
+
+                    // TODO 对不同的字段类型进行赋值
 //                    originalValue = busiLogService.getFieldValue(fieldName, originalValue);
 //                    modifyValue = busiLogService.getFieldValue(fieldName, modifyValue);
                     if (originalObj != null && originalObj.toString().trim().length() > 0)
@@ -169,19 +164,14 @@ public class ChangeLogAspect {
                     if ((originalValue != null && modifyValue != null && (!originalValue.equals(modifyValue)))
                             || (originalValue == null && modifyValue != null)
                             || (originalValue != null && modifyValue == null)) {
-
-
-                        // 保存到数据库
-                        System.out.println(original.getName() + "  " + originalValue + "   ======>   " + modifyValue);
+                        // TODO 保存到数据库
+                        System.out.println(fieldName + ": " + originalValue + "   ======>   " + modifyValue);
                     }
                 }
-
-                sqlSession.close();
             }
 
 
         }
-
     }
 
     /**
@@ -194,11 +184,11 @@ public class ChangeLogAspect {
         List<Method> listMethod = new ArrayList<Method>();
         for (Method tempMethod : allMethods) {
             String methodName = tempMethod.getName();
-            if (methodName.indexOf("get") != -1) {
-                if ((methodName.indexOf("getCreatedBy") != -1)
-                        || (methodName.indexOf("getCreatedTime") != -1)
-                        || (methodName.indexOf("getModifiedBy") != -1)
-                        || (methodName.indexOf("getModifiedTime") != -1)) {
+            if (methodName.contains("get")) {
+                if ((methodName.contains("getCreatedBy"))
+                        || (methodName.contains("getCreatedTime"))
+                        || (methodName.contains("getModifiedBy"))
+                        || (methodName.contains("getModifiedTime"))) {
                     continue;
                 }
                 Class[] parameterTypes = tempMethod.getParameterTypes();
