3d86840230b3f3f198039fa993565c515d6a8714
谭楮升 <tanchusheng@zto56.com>
2024-07-23 17:01:39 +0800

    feat: 创建型模式-单例模式

---
+++ design-pattern/creational/src/main/java/com/xxx/singleton/Singleton_00.java
@@ -0,0 +1,13 @@
+package com.xxx.singleton;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 静态类
+ */
+public class Singleton_00 {
+
+    public static Map<String,String> cache = new ConcurrentHashMap<String, String>();
+
+}
